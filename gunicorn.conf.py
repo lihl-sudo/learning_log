ce05b5f62f1c2be7d6c4e5e86b7a8db51112d34d
lihl-sudo <65524865+lihl-sudo@users.noreply.github.com>
2020-06-06 18:20:00 +0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,7 +5,7 @@ workers = multiprocessing.cpu_count() * 2 + 1    #进程数
 print(workers)
 errorlog = '/data/data/com.termux/files/home/sites/learning_log/gunicorn.error.log' #发生错误时log的路径
 loglevel = 'warning'
-# accesslog = '/home/xxx/xxx/gunicorn.access.log' #正常时的log路径
+accesslog = '/data/data/com.termux/files/home/sites/learning_log/gunicorn.log' #正常时的log路径
 proc_name = 'gunicorn_learningct'   #进程名
 timeout = 30      #超时
 threads = 2 #指定每个进程开启的线程数
