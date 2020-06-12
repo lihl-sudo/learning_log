8bc228a0f56c815156e9a14762bd8cbbcbe0ee01
lihaillong <li.hailong@hotmail.com>
2020-06-12 22:10:48 +0800

    修改init.sql注释

--- deployment/mysql/init/init.sql
+++ deployment/mysql/init/init.sql
@@ -1,6 +1,6 @@
---create database test;
---ALTER user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'lhl666666';
---CREATE USER 'lihl'@'%' IDENTIFIED WITH mysql_native_password BY 'lhl666666';
---GRANT All privileges ON test.* TO 'lihl'@'%';
---SELECT host, user, plugin, authentication_string FROM mysql.user;
---FLUSH PRIVILEGES;
+# create database test;
+# ALTER user 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'lhl666666';
+# CREATE USER 'lihl'@'%' IDENTIFIED WITH mysql_native_password BY 'lhl666666';
+# GRANT All privileges ON test.* TO 'lihl'@'%';
+# SELECT host, user, plugin, authentication_string FROM mysql.user;
+# FLUSH PRIVILEGES;
